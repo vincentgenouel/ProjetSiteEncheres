6df17dc981205cf0a8535cd4608b60c5ef6cae8c
Johanna-eni <johanna.rigollot2020@campus-eni.fr>
2020-08-25 08:30:31 +0200

    no message

--- src/fr/eni/ProjetSiteEncheres/dal/CodesResultatDAL.java
+++
@@ -1,5 +0,0 @@
-package fr.eni.ProjetSiteEncheres.dal;
-
-public abstract class CodesResultatDAL {
-
-}
--- src/fr/eni/ProjetSiteEncheres/dal/DAOFactory.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.ProjetSiteEncheres.dal;
-
-public class DAOFactory {
-	
-	public static ListeUtilisateurDAO getListeUtilisateurDAO() {
-
-	return new ListeUtilisateurDAOJdbcImpl();
-	}
-}
